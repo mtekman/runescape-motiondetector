f233a43c304db4f51558694ec9681ba51e80a39c
Mehmet Tekman <m.tekman@ucl.ac.uk>
2013-12-05 20:41:16 +0000

    window check exit

--- motion_detect.sh
+++ motion_detect.sh
@@ -4,12 +4,16 @@
 window="RuneScape"
 skeletonDect="./skeletonDetect/skeletonDetect"
 
+[ "$(xwininfo -name $window)" = "" ] && exit
+
+
 xpos=`xwininfo -name "$window" | grep Absol | awk '{ print $4 }' | head -1`
 ypos=`xwininfo -name "$window" | grep Absol | awk '{ print $4 }' | tail -1`
 width=`xwininfo -name "$window" | grep Width | awk '{ print $NF }' | head -1`
 height=`xwininfo -name "$window" | grep Height | awk '{ print $NF }' | head -1`
 
 topleft=`xwininfo -name "$window" | grep Corners | awk '{ print $2 }'`
+
 
 
 # Image location
