e754037b0408c02f966b399b6301bb4fed101b51
Mehmet Tekman <m.tekman@ucl.ac.uk>
2014-10-18 09:07:10 +0100

    cleanup

--- mineDetector/orefinder.h
+++ mineDetector/orefinder.h
@@ -18,7 +18,6 @@ struct OreFinder{
 
         filterPreviousBlacks(early, later, twink.keypoints, debug);
 
-        Mat king;
         if (debug){
             Mat debbie = early.clone()/2 + later.clone()/2;
             debbie /= 3;
@@ -32,21 +31,13 @@ struct OreFinder{
 
             debbie2 /= 1.5;
 
-            king = debbie + debbie2;
+            Mat king = debbie + debbie2;
             showIMG(king, 1000, 0);
 
             CVFuncs::addBlobVect2Image(twink.keypoints, king);
             showIMG(king, 1000, 0);
             showIMG(di.fgmask,1000,0);
         }
-
-
-//        if (debug){
-//            Mat debbie = early.clone();
-//            debbie /= 4;
-//            CVFuncs::addBlobVect2Image(twink.keypoints, debbie);
-//            showIMG(debbie, 900, 0);
-//        }
         ore_locs = twink.keypoints;
     }
 
@@ -58,14 +49,14 @@ struct OreFinder{
             Mat &early, Mat &later,
             keyvect &keypoints, bool &debug)
     {
+        int range = 5;
+        int step = 3;
+        float limit = 100;
+
         for (keyvect::iterator it= keypoints.begin();it != keypoints.end();)
         {
             KeyPoint &kp = *it;
             Point pt = kp.pt;
-
-            int range = 5;
-            int step = 3;
-            float limit = 100;
 
             if (debug){
                 cerr << endl;
@@ -74,7 +65,8 @@ struct OreFinder{
 
             Point min;
             float min_sq= 999999999999;
-            Vec3b chosen_set [2];
+//            Vec3b chosen_set [2];
+            bool good2go=false;
 
             for (int c=-range; c< range ;c += step){
                 for (int s= -range; s < range; s += step){
@@ -90,28 +82,30 @@ struct OreFinder{
                     if (b1 < limit || g1 < limit || r1 < limit
                             || b2 < limit || g2 < limit || r2 < limit){
 
-                        float square = c*c + s*s;
+/*                        float square = c*c + s*s;
                         if (square < min_sq){
                             min_sq = square;
                             min.x = c;
                             min.y = s;
                             chosen_set[0] = pixel_set1;
                             chosen_set[1] = pixel_set2;
-                        }
+                        }*/
+                        good2go = true;
                     }
                 }
             }
-            kp.pt.x += min.x;
-            kp.pt.y += min.y;
+//            kp.pt.x += min.x;
+//            kp.pt.y += min.y;
 
             if (debug){
                 cerr << "[" << kp.pt.x << "," << kp.pt.y << "] = " << flush;
-                cerr << (int)chosen_set[0][0] << " " << (int)chosen_set[0][1] << " " << (int)chosen_set[0][2] << "," << flush;
-                cerr << (int)chosen_set[1][0] << " " << (int)chosen_set[1][1] << " " << (int)chosen_set[1][2] << endl;
+//                cerr << (int)chosen_set[0][0] << " " << (int)chosen_set[0][1] << " " << (int)chosen_set[0][2] << "," << flush;
+//                cerr << (int)chosen_set[1][0] << " " << (int)chosen_set[1][1] << " " << (int)chosen_set[1][2] << endl;
             }
 
             //If neither are within lim, delete
-            if (chosen_set[0] == Vec3b({0,0,0}))
+//            if (chosen_set[0] == Vec3b({0,0,0}))
+            if (!good2go)
             {
                 it = keypoints.erase(it); //give next iterator
             }
